3c799161daf8c6bf99ce7ed01b1cef159191e46a
sammydeluxe <farbstoffe_wagen_0k@icloud.com>
2023-12-27 15:15:48 +0100

    add function and messages

---
+++ messageGenerator.js
@@ -0,0 +1,18 @@
+
+const message = [
+    'Wenn es heute regnet ist es morgen nass.',
+    'Die Sonne scheint, wenn es nicht regnet.',
+    'Kehre in dich und du wirst die Antwort finden.',
+    'Wenn du nicht weißt, was du tun sollst, dann tu nichts.',
+    'Wie wäre es sich einfach mal zu entspannen?',
+    'Ein wenig Bildung schadet nie.',
+    'Zu Coden macht Spass, aber nur wenn es funktioniert.',
+]
+
+
+const messageGenerator = () => {
+    const index = Math.floor(Math.random() * message.length);
+    return message[index];
+}
+
+console.log(messageGenerator());
